b633588dc70cc59d36b72e0b9f6229a4867c6c85
Gerhard <gerhard.putter@yahoo.com>
2021-08-22 19:28:40 +0200

    Added animation.

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
-
+import { motion } from 'framer-motion'
 import { CarouselButton, CarouselButtonDot, CarouselButtons, CarouselContainer, CarouselItem, CarouselItemImg, CarouselItemText, CarouselItemTitle, CarouselMobileScrollNode } from './TimeLineStyles';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { TimeLineData } from '../../constants/constants';
@@ -9,15 +9,16 @@ const TOTAL_CAROUSEL_COUNT = TimeLineData.length;
 const Timeline = () => {
 
   return (
-    <Section id="about">
-      <SectionDivider />
-      <br />
-      <SectionTitle>About Me</SectionTitle>
-      <SectionText>
-        I am a Front-End web developer based in Cape Town, South Africa. My goal is to write well designed code that is testable and efficient by using best development practices. I have been actively building small apps and working on my own bigger projects for the past 8 months.
-      </SectionText>
-
-    </Section>
+    <motion.div animate={{ y: 0 }} initial={{ y: 320 }} transition={{ duration: 3 }}>
+      <Section id="about">
+        <SectionDivider />
+        <br />
+        <SectionTitle>About Me</SectionTitle>
+        <SectionText>
+          I am a Front-End web developer based in Cape Town, South Africa. My goal is to write well designed code that is testable and efficient by using best development practices. I have been actively building small apps and working on my own bigger projects.
+        </SectionText>
+      </Section>
+    </motion.div >
   );
 };
 
