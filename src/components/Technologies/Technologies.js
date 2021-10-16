d22f43fc6f16fd9cbc7eaea36942916ba3895254
Gerhard <gerhard.putter@yahoo.com>
2021-10-16 19:17:31 +0200

    Added framer mortion to Stack section.

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -2,16 +2,19 @@ import React from 'react';
 import { FaHtml5, FaReact, FaSass } from 'react-icons/fa';
 import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import { motion } from 'framer-motion'
 
 const Technologies = () => (
-  <Section id="tech">
+  <motion.div animate={{ y: 0 }} initial={{ y: 320 }} transition={{ duration: 3 }}>
+    <Section id="tech">
 
-    <SectionTitle>Stack</SectionTitle>
-    <SectionText>
-      I have experience working with the following technologies: HTML, CSS, Javascript, Sass, Bootstrap, React, Next.js, Git, Github.
-    </SectionText>
+      <SectionTitle>Stack</SectionTitle>
+      <SectionText>
+        I have experience working with the following technologies: HTML, CSS, Javascript, Sass, Bootstrap, React, Next.js, Git, Github.
+      </SectionText>
 
-  </Section>
+    </Section>
+  </motion.div >
 );
 
 export default Technologies;
